05934669b5745c14179acddcda2d2edca4b0c57b
potassium-platinums <59970163+potassium-platinums@users.noreply.github.com>
2025-07-24 15:59:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,92 @@
 const mqtt = require('mqtt');
 const axios = require('axios');
 
-const mqttClient = mqtt.connect('mqtt://broker.hivemq.com');
+
+// mgtt user data
+const brokerURL = 'mqtt://mqtt.koinsightug.com';
+const username = 'gkfiqxkh';
+const password = 'wtc48z8dSovj';
+
+// Connection options
+const options = {
+    port: 1883,
+    username: username,
+    password: password
+};
+
+const mqttClient = mqtt.connect(brokerURL, options); 
 
 mqttClient.on('connect', () => {
     console.log('Connected to MQTT broker');
-    mqttClient.subscribe('sensor/data'); // Topic you are interested in
-});
-
-mqttClient.on('message', (topic, message) => {
-    let data = JSON.parse(message.toString());
-    // console.log(data);
-   axios.post('https://coffee.rndnakawa.com/insert.php', {
-        id: "ojok",
-        name: "okumu",
-        email: "ronald"
-    }).then(res => {
-        console.log("Data sent to PHP");
-    }).catch(err => {
-        console.error("Error sending data to PHP", err);
+    // Subscribe to topics
+    mqttClient.subscribe('woodCount/+/content', (err) => {
+        if (!err) {
+            // console.log(`Subscribed to topic`);
+        }
+    });
+    mqttClient.subscribe('woodCount/+/state', (err) => {
+        if (!err) {
+            // console.log(`Subscribed to topic`);
+        }
     });
 
 
+});
+
+
+
+// Global object to track state per device
+let deviceStates = {};  // { device_id: status }
+
+mqttClient.on('message', async (topic, message) => {
+    try {
+        let mqtt_data = JSON.parse(message.toString());
+        // console.log(mqtt_data);
+    
+        let deviceID = mqtt_data.device_id;
+        let deviceState = mqtt_data.status;
+        let deviceCount = mqtt_data.count;
+        let deviceDate = mqtt_data.date;
+        let deviceTime = mqtt_data.time;
+        let deviceLocation = mqtt_data.location;
+
+        // Only proceed if deviceID is present
+        if (!deviceID) return;
+
+        // Update device state if provided
+        if (deviceState !== undefined) {
+            deviceStates[deviceID] = deviceState;
+            // send other data
+            axios.post('https://coffee.rndnakawa.com/api/insert_status.php', {
+                id: deviceID,
+                status: deviceState,
+            }).then(res => {
+                console.log("Data sent to PHP:", res.data);
+            }).catch(err => {
+                console.error("Error sending data to PHP", err);
+            });
+            
+        }
+
+        // If we received count, date, time, and location, proceed to update logic
+        if (deviceCount !== undefined && deviceDate && deviceTime && deviceLocation) {
+
+            // send other data
+            axios.post('https://coffee.rndnakawa.com/api/insert_data.php', {
+                id: deviceID,
+                count: deviceCount,
+                date: deviceDate,
+                time: deviceTime,
+                location: deviceLocation,
+            }).then(res => {
+                console.log("Data sent to PHP:", res.data);
+            }).catch(err => {
+                console.error("Error sending data to PHP", err);
+            });
+        }
+
+
+    } catch (error) {
+        console.error("Error handling MQTT message:", error);
+    }
 });
